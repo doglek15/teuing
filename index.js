fe3edb701b37bf59c18a4f728f481b5059433e34
dog <84648772+doglek15@users.noreply.github.com>
2024-02-23 18:13:00 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+
+// Fungsi untuk menggabungkan dua string
+function concatenateStrings(str1, str2) {
+  return str1 + str2;
+}
+
+// Fungsi untuk mengubah string menjadi huruf kecil
+function toLowerCase(str) {
+  return str.toLowerCase();
+}
+
+module.exports = {
+  concatenateStrings,
+  toLowerCase
+};
